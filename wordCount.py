57f941c84fda41b98fb6c9404d43dc8f294f5bb2
Ricardo Alvarez <35704560+ralvarezlo@users.noreply.github.com>
2018-09-03 13:08:38 -0600

    Fixed punctuation

--- wordCount.py
+++ wordCount.py
@@ -17,13 +17,22 @@ if not os.path.exists(inName):
     print ("Input %s doesn't exist." % inName)
     exit()
 
+# Check output and delete if necessary
+if os.path.exists(outName):
+    os.remove(outName)
+    print("removing output file")
+
 wordDic = {}
-addW = False
 
 with open(inName, 'r') as inputFile:
     for line in inputFile:
         # get rid of newline characters
         line = line.strip()
+        #change hyphens to spaces
+        line = line.replace("-", " ")
+        #remove punctuation
+        for i in string.punctuation:
+            line = line.replace(i, "")
 
         # split line on whitespace
         auxList = list(line.split(" "))
@@ -32,35 +41,20 @@ with open(inName, 'r') as inputFile:
             # set to lowercase
             auxList[i] = auxList[i].lower()
 
-            # remove punctuation
-            strLength = len(auxList[i])
-            if strLength>=1:
-                # discard string if it's just punctuation
-                if (auxList[i][0].isalpha()): addW = True
-                # if it has punctuation at the end, remove it
-                if not (auxList[i][strLength-1].isalpha()):
-                    auxList[i] = auxList[i][:(strLength-1)]
-
-            # add to dictionary or add count
-            if addW:
+            if len(auxList[i])>=1 and auxList[i][0].isalpha():
+                # add to dictionary or to count
                 if not auxList[i] in wordDic:
                     wordDic[auxList[i]] = 1
                 else:
                     wordDic[auxList[i]] += 1
-            addW = False
 
 # Dictionary Sort
 outDic = {}
 for i in sorted(wordDic):
-    outDic[i] = wordDic[i]  
+    outDic[i] = wordDic[i]
 
-for i in outDic:
-    print(i, outDic[i], sep=' ')
-
-print("\nWriting to file...")
-
+# Write to file
 fileOut = open(outName, "w")
 for i in outDic:
     fileOut.write(str(i) + " " + str(outDic[i]) + "\n")
 fileOut.close()
-print("\nWriting done!")
