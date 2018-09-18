26bfe72f046c219466eabb0351288722e0ffff0c
Ricardo <ralvarezlo@miners.utep.edu>
2018-09-17 20:43:11 -0600

    Pipe (Must Fix W,R)

--- shelllLab/shellFromP4.py
+++ shelllLab/shellFromP4.py
@@ -27,12 +27,17 @@ def setIns(auxIn):
                 print("Invalid Argument")
                 sys.exit(1)
         i += 1
-
     return uIn, out
 
+def mustPipe (auxStr):
+    isPipe = False
+    auxArr = auxStr.split("|")
+    if len(auxArr) > 1:
+        isPipe = True
+    return isPipe, auxArr
 
-auxStr = input("Please enter input: ")
-print("string input is " + auxStr)
+auxStr = input("$ ")
+iPipe, auxArr = mustPipe(auxStr)
 myIn = auxStr.split(" ")
 
 pid = os.getpid()               # get and remember pid
@@ -49,6 +54,55 @@ elif rc == 0:                   # child
     os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
                  (os.getpid(), pid)).encode())
     # args = ["wc", "p3-exec.py"]
+
+    if iPipe:
+        w, r = os.pipe()
+        # FIX THIS
+
+        pid = os.getpid()               # get and remember pid
+        os.write(1, ("About to fork for pipe (pid=%d)\n" % pid).encode())
+        fNum = os.fork()
+
+        if fNum == 0: # child for pipe
+            args, uOut = setIns(auxArr[0])
+            os.close(1)
+            w = os.fdopen(w, 'w')
+            fd = w.fileno()
+            os.set_inheritable(fd, True)
+            os.write(2, ("ChildPIPE: opened fd=%d for writing\n" % fd).encode())
+            
+
+            for dir in re.split(":", os.environ['PATH']): # try each directory in path
+                programIn = "%s/%s" % (dir, args[0])
+                try:
+                    os.execve(programIn, args, os.environ) # try to exec program
+                except FileNotFoundError:             # ...expected
+                    pass
+            sys.exit(1)
+            
+        else:
+            args, uOut = setIns(auxArr[1])
+            childPidCode = os.wait()
+            r = os.fdopen(r, 'r')
+            args[1] = r.read()
+            if (uOut != "p4-output.txt"):
+                print("output is " + uOut)
+                os.close(1)                 # redirect child's stdout
+                sys.stdout = open(uOut, "w")
+                fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
+                os.set_inheritable(fd, True)
+                os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
+            for dir in re.split(":", os.environ['PATH']): # try each directory in path
+                program = "%s/%s" % (dir, args[0])
+                try:
+                    os.execve(program, args, os.environ) # try to exec program
+                    sys.exit(1)
+                except FileNotFoundError:             # ...expected
+                    pass                              # ...fail quietly 
+
+            os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
+            sys.exit(1)                 # terminate with error
+
     args, uOut = setIns(myIn)
     
     if (uOut != "p4-output.txt"):
