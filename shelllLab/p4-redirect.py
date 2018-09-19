f6f4b02988b5aca00e99e61257fbb3009eab6e63
Ricardo <ralvarezlo@miners.utep.edu>
2018-09-19 17:44:38 -0600

    Added Enter, Ps1 and cd command support

--- shelllLab/p4-redirect.py
+++ shelllLab/p4-redirect.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python3
 
-import os, sys, time, re
+import os
+import re
+import sys
+import time
 
 pid = os.getpid()               # get and remember pid
 
--- shelllLab/shell.py
+++ shelllLab/shell.py
@@ -9,7 +9,6 @@ def setIns(auxStr):
     auxIn = auxStr.split(" ")
     uIn = [auxIn[0]]
     out = "p4-output.txt"
-
     i = 1
     while i < len(auxIn):
         if auxIn[i] == ">":
@@ -53,16 +52,13 @@ toClose: 1 or 0 (The fileDescriptor that will be closed)
 w, r: File descriptors opened by the pipe command to write and read respectively
 """
 def setFds(toClose, w, r):
-    os.write(2, ("To Close: %d, W: %d, R: %d\n" %(toClose, w, r)).encode())
     os.close(toClose)
     # if we close #1 we know we will want to dup the writting fd
     if (toClose==1):
         #fd = w
-        os.write(2, ("Closed: %d FD: %d" %(toClose, fd)).encode())
         os.dup2(w, 1)
     else: 
         #fd = r
-        os.write(2, ("Closed: %d FD: %d" %(toClose, fd)).encode())
         os.dup2(r, 0)
     
     
@@ -75,8 +71,6 @@ def setFds(toClose, w, r):
 Executes a command given the arguments (Given by the professor)
 """
 def excIt(args):
-    print("Inside Exec: " + args[0])
-    os.write(2,("\nArgs: " + args[0]+"\n").encode())
     for dir in re.split(":", os.environ['PATH']): # try each directory in path
         programIn = "%s/%s" % (dir, args[0])
         try:
@@ -90,17 +84,18 @@ it takes the fileName and sets it as the 'new' stdout
 """
 def setOutFile(uOut):
     if (uOut != "p4-output.txt"):
-        print("output is " + uOut)
         os.close(1)                 # redirect child's stdout
         sys.stdout = open(1, "w")
         fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
         os.set_inheritable(fd, True)
-        os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
 
+def changeDirectory(path):
+    try:
+        os.chdir(path)
+    except:
+        os.write(3, ("\nIncorrect location\n").encode())
 
-done = False
-while not done:
-    auxStr = input("$ ")
+def mainRun(auxStr):
     if (auxStr == "exit"): sys.exit(1)
     doPipe, myIns = mustPipe(auxStr)
     pid = os.getpid()
@@ -108,7 +103,6 @@ while not done:
         pr, pw = os.pipe()
         for fd in (pr, pw):
             os.set_inheritable(fd, True)
-        os.write(2, ("pids for pip pr={0} pw={1}\n".format(pr,pw)).encode())
 
     fork1 = os.fork()
     if fork1 < 0:
@@ -116,8 +110,10 @@ while not done:
         sys.exit(1)
 
     elif fork1 == 0:                   # child1
+        """
         os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
                     (os.getpid(), pid)).encode())
+                    """
         args, myOut = setIns(myIns[0])
         if doPipe:
             setFds(1, pw, pr)
@@ -138,11 +134,34 @@ while not done:
                 #setOutFile(myOut)
                 excIt(args) 
             else:
-                os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % 
-                        (pid, fork2)).encode())
                 childPidCode = os.wait()
 
                 for pipefd in (pw, pr):
                     os.close(pipefd)
-
         child2Pid = os.wait()
+
+ 
+done = False
+while not done:
+    try:
+        if 'PS1' in os.environ:
+            os.write(1, os.environ['PS1'].encode())
+            auxStr = input("")
+        else: auxStr = input("$ ")
+        run = True
+        if(auxStr==""): run = False #Handle enters
+        if(auxStr[0:2]=="cd"):
+            try:
+                changeDirectory(auxStr[3:])
+            except:
+                print("Incorrect Path")
+                pass
+            run = False
+        
+    except EOFError: #catch ctr + D
+        print("\n") #avoid the annoying shell log after close
+        sys.exit(1)
+    
+    if(run):
+        mainRun(auxStr)
+    
